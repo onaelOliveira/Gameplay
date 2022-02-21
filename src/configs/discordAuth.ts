c9a6c6570fb7e99cdba5d5619fde27e60ce5bb60
onaelOliveira <54674136+onaelOliveira@users.noreply.github.com>
2022-02-21 15:47:52 -0300

    nlw

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,15 @@
+const REDIRECT_URI = 'https%3A%2F%2Fauth.expo.io%2Fgameplay';
+const SCOPE = 'identify%20email%20connections%20guilds';
+const RESPONSE_TYPE = 'token';
+const CLIENTE_ID = '935248746302832660';
+const CDN_IMAGE = 'https:cdn.discordapp.com'
+
+
+export {
+    REDIRECT_URI,
+    SCOPE,
+    RESPONSE_TYPE,
+    CLIENTE_ID,
+    CDN_IMAGE
+}
+
